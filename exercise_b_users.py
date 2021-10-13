88e1b38352c1ab5b2ced693b46e707a31a0f4ea3
Sam <samspencer2202@outlook.com>
2021-10-13 03:14:25 +0100

    answer code complete

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -64,3 +64,42 @@ users = {
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users dictionary
+
+print("\n")
+print("Jonathans twitter is: ", users["Jonathan"]["twitter"])                     # prints Jonathans twitter handle
+
+print("Erik's hometown is: ", users["Erik"]["home_town"])                         # prints Erik's hometown
+
+print("Erik's Lotto numbers are: ", users["Erik"]["lottery_numbers"])             # prints Eriks Lottery numbers
+
+avrilPet = users["Avril"]["pets"][0]["species"]             # stores the species of avrilsPet in a new variable
+print("Avrils pet is a: ", avrilPet, "\n")                  # and prints it
+
+erikLottoList = users["Erik"]["lottery_numbers"]                                # initialises new List containing Eriks lottery no's.
+print("Lowest number in Erik's numbers is: ", min(erikLottoList), "\n")         # and prints the lowest number.
+
+avrilLottoList = users["Avril"]["lottery_numbers"]          # initialises new List containing Avrils lottery no's.
+avrilEven = []                                              # initialises empty List 
+for num in avrilLottoList:                                  
+    if num % 2 == 0:                                        # for all the items in avrils lottery numbers, IF they divide by 2 with no remainder --
+        avrilEven.append(num)                               # They are EVEN. if they are EVEN they are added to the empty List from before.
+print("Avrils EVEN lotto numbers: ", avrilEven, '\n')       # new List is then printed
+
+erikLottoList.append(7)                                                         # add no. 7 to Eriks numbers.
+print("Erik has added a number: ", erikLottoList, "\n")
+
+erikHome = users["Erik"]["hometown"] = "Edinburgh"                              # new variable = Eriks hometown, which is also being reset to 'Edinburgh'
+print("Erik's new hometown is: ", erikHome, "\n")                               # new variable then printed
+
+erikPet = users["Erik"]["pets"]                                                 # new List containing Erik's pets
+erikPet.append({"name":"Fluffy", "species":"dog"})                              # adding new pet to dictionary within a list within a dictionary within a list within.........
+print("Erik's pets with newly added DOG: ", erikPet, "\n")                      # Print List of pets to show newly added Dog.
+
+users["Sam"] = {"twitter":"samS", "lottery_numbers":[1,2,3,4,5,6,7], "home_town":"Linlithgow", "pets":[{"name":"leia", "species":"dog"}]}
+for key, value in users.items() :
+  if key == "Sam":                                                              # new user added above here, all in one line. could be more elegant!
+    print ("New user added: ", key, value)                                      # loop with conditional used to select the new user by KEY then display all values within.
+
+
+
+
